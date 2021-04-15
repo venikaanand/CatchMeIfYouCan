1f4756d96bdf1c43969de57b9f5d3a504781c062
Varma CU <ck3025@columbia.edu>
2021-04-15 18:31:44 -0400

    add urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 apps_name = 'sightings'
 urlpatterns = [
-    path('', views.website_homepage, name='website_homepage')
+    path('', views.website_homepage, name='website_homepage'),
+	path('map/', views.squirrel_map, name='squirrel_map'),
+	path('sightings/', views.squirrel_list, name='squirrel_list')
 ]
