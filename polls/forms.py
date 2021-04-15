442add6e0afb4abd39878e9b7b070111d2528ecb
Varma CU <ck3025@columbia.edu>
2021-04-15 18:33:05 -0400

    add forms

--- polls/forms.py
+++ polls/forms.py
@@ -7,3 +7,9 @@ class SquirrelAllForm(ModelForm):
 		model = SquirrelModel
 		fields = '__all__'
 		#fields = ['Latitude', 'Longitude', 'Unique_Squirrel_ID', 'Shift','Date','Age']
+
+class SquirrelAddForm(ModelForm):
+	class Meta:
+		model = SquirrelModel
+		#fields = '__all__'
+		fields = ['Latitude', 'Longitude', 'Unique_Squirrel_ID', 'Shift','Date','Age','Primary_Fur_Color','Location','Specific_Location','Running','Chasing', 'Climbing', 'Eating','Foraging','Other_Activities','Kuks','Quaas','Moans',  'Tail_Flags', 'Tail_Twitches', 'Approaches', 'Indifferent',  'Runs_From']
