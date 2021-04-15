78b1ab759eed2948ead96b39e039566a694d2e3c
Venika Anand <va2439@columbia.edu>
2021-04-15 18:34:14 -0400

    dding sightings > with list connected to db and add features

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 apps_name = 'sightings'
 urlpatterns = [
-    path('', views.website_homepage, name='website_homepage')
-
+    path('', views.website_homepage, name='website_homepage'),
+	path('map/', views.squirrel_map, name='squirrel_map'),
+	path('sightings/', views.squirrel_list, name='squirrel_list'),
+    path('sightings/stats/', views.squirrel_stats, name = 'squirrel_stats'),
+    path('sightings/add/', views.squirrel_add, name = 'squirrel_add'),
+    path('sightings/<str:squirrel_id>/', views.squirrel_details, name='squirrel_details'),
+	path('sightings/<str:squirrel_id>/update', views.squirrel_modify, name='squirrel_modify'),
 ]
